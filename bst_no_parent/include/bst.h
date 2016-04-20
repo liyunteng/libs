71c30cf00bbe72428200b6dc3ce41a45e1c8b00f
liyunteng <li_yunteng@163.com>
2016-04-20 08:24:23 +0800

    add makefile for bst_no_parent

---
+++ bst_no_parent/include/bst.h
@@ -0,0 +1,45 @@
+/*
+ * bst.h --
+ *
+ * Copyright (C) 2016 liyunteng
+ * Auther: liyunteng <li_yunteng@163.com>
+ * License: GPL
+ * Update time:  2016/04/20 06:44:01
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef BST_H
+#define BST_H
+#include "types.h"
+
+typedef int datatype;
+struct bst_node {
+	struct bst_node *left;
+	struct bst_node *right;
+	datatype data;
+};
+
+extern struct bst_node *bst_insert(struct bst_node **root, datatype data);
+extern struct bst_node *bst_create(struct bst_node **root, datatype  a[], int len);
+extern void bst_pre_iterate(struct bst_node *root);
+extern void bst_in_iterate(struct bst_node *root);
+extern void bst_post_iterate(struct bst_node *root);
+extern struct bst_node *bst_min(struct bst_node *root);
+extern struct bst_node *bst_max(struct bst_node *root);
+extern struct bst_node *bst_search(struct bst_node *root, datatype key);
+extern int bst_delete(struct bst_node **root, datatype key);
+
+#endif
---
+++ bst_no_parent/test/test_bst.c
@@ -0,0 +1,77 @@
+/*
+ * test_bst.c -- bst test
+ *
+ * Copyright (C) 2016 liyunteng
+ * Auther: liyunteng <li_yunteng@163.com>
+ * License: GPL
+ * Update time:  2016/04/20 08:20:30
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "bst.h"
+
+int main(void)
+{
+	struct bst_node *root = NULL;
+	datatype arr[] = {2, 1, 7, 4, 3, 6, 5, 8};
+
+	/*
+	 * datatype arr[4096];
+	 * srandom(time(NULL));
+	 * for (int i = 0; i < (int)ARRAY_SIZE(arr); i++) {
+	 *	arr[i] = random() % 100;
+	 * }
+	 */
+	root = bst_create(&root, arr, ARRAY_SIZE(arr));
+	for (int i = 0; i < (int)ARRAY_SIZE(arr); i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	int ret = 0;
+	ret = bst_delete(&root, 4);
+	assert(ret == 0);
+	ret = bst_delete(&root, 5);
+	assert(ret == 0);
+	ret = bst_delete(&root, 7);
+	assert(ret == 0);
+	ret = bst_delete(&root, 8);
+	assert(ret == 0);
+
+	printf("pre:\n");
+	bst_pre_iterate(root);
+	/*
+	 * printf("\nin:\n");
+	 * bst_in_iterate(root);
+	 * printf("\npost:\n");
+	 * bst_post_iterate(root);
+	 */
+
+	printf("\nmin: %d, max: %d\n", bst_min(root)->data, bst_max(root)->data);
+
+	struct bst_node *n = bst_search(root, 5);
+	if (n) {
+		printf("%d found.\n", n->data);
+	} else {
+		printf("not found.\n");
+	}
+
+	return 0;
+
+}

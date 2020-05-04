2d0a70ef112cea43abbf05e2877140884a369912
liyunteng <li_yunteng@163.com>
2020-05-04 22:40:17 +0800

    update xmlcfg test

--- xmlcfg/test/test.c
+++ xmlcfg/test/test.c
@@ -33,9 +33,10 @@ const char *xml_template = "                                        \
                     <!-- <timeshift>-1</timeshift> -->              \
                     <size>32k</size>                                \
                 </file>                                             \
-<file>                                                              \
-<name> http://www.baidu.com </name>                                 \
-</file>                                                             \
+                <file>                                              \
+                    <name> http://www.baidu.com </name>             \
+                    <size>1M</size>                                 \
+                </file>                                             \
             </files>                                                \
         </content>                                                  \
     </upload>                                                       \
@@ -43,6 +44,7 @@ const char *xml_template = "                                        \
 </root>                                                             \
 ";
 
+int test1(void);
 int
 test1()
 {
@@ -58,11 +60,6 @@ test1()
         return -1;
     }
     printf("upload.content.guid = %s\n", p);
-    if (xmlcfg_get_str(cfg, "upload.content.files.file.name", &p) != 0) {
-        ERR("xmlcfg_get_str");
-        return -1;
-    }
-    printf("upload.content.files.file.name = %s\n", p);
 
     int64_t val = 0;
     if (xmlcfg_get_int(cfg, "upload.content.priority", &val) != 0) {
@@ -71,12 +68,29 @@ test1()
     }
     printf("upload.content.priority = %ld\n", val);
 
-    size_t size = 0;
-    if (xmlcfg_get_size(cfg, "upload.content.files.file.size", &size) != 0) {
-        ERR("xmlcfg_get_size");
+
+    xmlcfg_ptr iter = NULL;
+    if ((iter = xmlcfg_iter_init(cfg, "upload.content.files", "file")) == NULL) {
+        ERR("xmlcfg iter init failed\n");
         return -1;
     }
-    printf("upload.content.files.file.size = %lu\n", size);
+    do {
+        if (xmlcfg_get_str(iter, "name", &p) != 0) {
+            ERR("xmlcfg_get_str");
+            return -1;
+        }
+        printf("upload.content.files.file.name = %s\n", p);
+
+        size_t size = 0;
+        if (xmlcfg_get_size(iter, "size", &size) != 0) {
+            ERR("xmlcfg_get_size");
+            return -1;
+        }
+        printf("upload.content.files.file.size = %lu\n", size);
+
+        iter = xmlcfg_iter_next(iter);
+    } while (xmlcfg_iter_hasnext(iter));
+
     return 0;
 }
 
